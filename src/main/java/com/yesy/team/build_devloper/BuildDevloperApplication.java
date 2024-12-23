be9a90058f4f025644d1b5200ff4f008a069a4d7
qBlackbirdp <blackbird9711@gmail.com>
2024-12-23 16:01:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/yesy/team/build_devloper/BuildDevloperApplication.java
@@ -0,0 +1,13 @@
+package com.yesy.team.build_devloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildDevloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildDevloperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yesy/team/build_devloper/BuildDevloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yesy.team.build_devloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildDevloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
